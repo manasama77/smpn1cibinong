801abcb18476e10e4bb0c17f04e70630a145d5e2
Adam PM <adam.pm77@gmail.com>
2023-10-23 17:48:19 +0700

    finihsing versi 1

--- DB/smpn1cibinongdb.sql
+++ DB/smpn1cibinongdb.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80100 (8.1.0)
  File Encoding         : 65001
 
- Date: 23/10/2023 15:22:43
+ Date: 23/10/2023 17:37:20
 */
 
 SET NAMES utf8mb4;
@@ -98,11 +98,13 @@ CREATE TABLE `jawaban_ujians`  (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of jawaban_ujians
 -- ----------------------------
+INSERT INTO `jawaban_ujians` VALUES (1, 1, 1, 'pg', 'a', 1, 0, '2023-10-23 16:49:37', '2023-10-23 16:49:37');
+INSERT INTO `jawaban_ujians` VALUES (2, 1, 2, 'essay', 'lorem iaoglnaklgnklds', 0, 4, '2023-10-23 16:49:37', '2023-10-23 17:33:44');
 
 -- ----------------------------
 -- Table structure for master_bank_soals
@@ -123,11 +125,12 @@ CREATE TABLE `master_bank_soals`  (
   `updated_by` bigint UNSIGNED NULL DEFAULT NULL,
   `deleted_by` bigint UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of master_bank_soals
 -- ----------------------------
+INSERT INTO `master_bank_soals` VALUES (1, 1, '7', '2023-10-23 00:00:00', '2023-10-26 23:59:00', 1, '653635e6e4456', '2023-10-23 16:02:40', '2023-10-23 16:02:40', NULL, 2, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for master_mapels
@@ -148,9 +151,9 @@ CREATE TABLE `master_mapels`  (
 -- ----------------------------
 -- Records of master_mapels
 -- ----------------------------
-INSERT INTO `master_mapels` VALUES (1, 'Bahasa Indonesia', NULL, '2023-10-23 15:21:49', '2023-10-23 15:21:49', 1, NULL, NULL);
-INSERT INTO `master_mapels` VALUES (2, 'Bahasa Inggris', NULL, '2023-10-23 15:21:49', '2023-10-23 15:21:49', 1, NULL, NULL);
-INSERT INTO `master_mapels` VALUES (3, 'Matematika', NULL, '2023-10-23 15:21:49', '2023-10-23 15:21:49', 1, NULL, NULL);
+INSERT INTO `master_mapels` VALUES (1, 'Bahasa Indonesia', NULL, '2023-10-23 15:51:58', '2023-10-23 15:51:58', 1, NULL, NULL);
+INSERT INTO `master_mapels` VALUES (2, 'Bahasa Inggris', NULL, '2023-10-23 15:51:58', '2023-10-23 15:51:58', 1, NULL, NULL);
+INSERT INTO `master_mapels` VALUES (3, 'Matematika', NULL, '2023-10-23 15:51:58', '2023-10-23 15:51:58', 1, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for migrations
@@ -161,24 +164,24 @@ CREATE TABLE `migrations`  (
   `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 244 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 270 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of migrations
 -- ----------------------------
-INSERT INTO `migrations` VALUES (231, '2014_10_12_000000_create_users_table', 1);
-INSERT INTO `migrations` VALUES (232, '2014_10_12_100000_create_password_reset_tokens_table', 1);
-INSERT INTO `migrations` VALUES (233, '2014_10_12_100000_create_password_resets_table', 1);
-INSERT INTO `migrations` VALUES (234, '2019_08_19_000000_create_failed_jobs_table', 1);
-INSERT INTO `migrations` VALUES (235, '2019_12_14_000001_create_personal_access_tokens_table', 1);
-INSERT INTO `migrations` VALUES (236, '2023_10_21_114547_create_master_bank_soals_table', 1);
-INSERT INTO `migrations` VALUES (237, '2023_10_21_114606_create_sub_bank_soals_table', 1);
-INSERT INTO `migrations` VALUES (238, '2023_10_21_114618_create_pg_bank_soals_table', 1);
-INSERT INTO `migrations` VALUES (239, '2023_10_21_114634_create_master_mapels_table', 1);
-INSERT INTO `migrations` VALUES (240, '2023_10_21_114715_create_blogs_table', 1);
-INSERT INTO `migrations` VALUES (241, '2023_10_21_114721_create_galleries_table', 1);
-INSERT INTO `migrations` VALUES (242, '2023_10_21_114733_create_ujians_table', 1);
-INSERT INTO `migrations` VALUES (243, '2023_10_21_114741_create_jawaban_ujians_table', 1);
+INSERT INTO `migrations` VALUES (257, '2014_10_12_000000_create_users_table', 1);
+INSERT INTO `migrations` VALUES (258, '2014_10_12_100000_create_password_reset_tokens_table', 1);
+INSERT INTO `migrations` VALUES (259, '2014_10_12_100000_create_password_resets_table', 1);
+INSERT INTO `migrations` VALUES (260, '2019_08_19_000000_create_failed_jobs_table', 1);
+INSERT INTO `migrations` VALUES (261, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+INSERT INTO `migrations` VALUES (262, '2023_10_21_114547_create_master_bank_soals_table', 1);
+INSERT INTO `migrations` VALUES (263, '2023_10_21_114606_create_sub_bank_soals_table', 1);
+INSERT INTO `migrations` VALUES (264, '2023_10_21_114618_create_pg_bank_soals_table', 1);
+INSERT INTO `migrations` VALUES (265, '2023_10_21_114634_create_master_mapels_table', 1);
+INSERT INTO `migrations` VALUES (266, '2023_10_21_114715_create_blogs_table', 1);
+INSERT INTO `migrations` VALUES (267, '2023_10_21_114721_create_galleries_table', 1);
+INSERT INTO `migrations` VALUES (268, '2023_10_21_114733_create_ujians_table', 1);
+INSERT INTO `migrations` VALUES (269, '2023_10_21_114741_create_jawaban_ujians_table', 1);
 
 -- ----------------------------
 -- Table structure for password_reset_tokens
@@ -250,11 +253,12 @@ CREATE TABLE `pg_bank_soals`  (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of pg_bank_soals
 -- ----------------------------
+INSERT INTO `pg_bank_soals` VALUES (1, 1, 'Oreo', 'Sari Gandum', 'Biskuat', 'Silver Queen', 'Regal', 'a', '2023-10-23 16:01:38', '2023-10-23 16:01:38');
 
 -- ----------------------------
 -- Table structure for sub_bank_soals
@@ -271,11 +275,13 @@ CREATE TABLE `sub_bank_soals`  (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sub_bank_soals
 -- ----------------------------
+INSERT INTO `sub_bank_soals` VALUES (1, 1, 'pg', 'Gambar apa ini', 'soal/U8wEcx5oHUL7yWo9x2Pds9iNGRbJl3vfcmikMJ1v.jpg', 0, '653635e6e4456', '2023-10-23 16:01:38', '2023-10-23 16:02:40');
+INSERT INTO `sub_bank_soals` VALUES (2, 1, 'essay', 'Jelaskan secara singkat tentang kamu', NULL, 4, '653635e6e4456', '2023-10-23 16:02:20', '2023-10-23 16:02:40');
 
 -- ----------------------------
 -- Table structure for ujians
@@ -291,11 +297,12 @@ CREATE TABLE `ujians`  (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of ujians
 -- ----------------------------
+INSERT INTO `ujians` VALUES (1, 3, 1, 5, 1, 4, '2023-10-23 16:49:37', '2023-10-23 17:33:44');
 
 -- ----------------------------
 -- Table structure for users
@@ -322,11 +329,13 @@ CREATE TABLE `users`  (
   `deleted_by` int NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `users_email_unique`(`email` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (1, 'admin@test.test', '$2y$10$MEMqOq2qqW0ypv12tn8yL.3E5I7eHmo/IKwNpIPsTuLq/dvetr6EK', 'Admin', NULL, 'admin', NULL, '082114578976', NULL, NULL, NULL, NULL, '2023-10-23 15:21:49', '2023-10-23 15:21:49', NULL, 1, NULL, NULL);
+INSERT INTO `users` VALUES (1, 'admin@test.test', '$2y$10$F3oF2cOaVjFLLvo0Z7mVz.Gx9nHDX3iDivYC//yxk/jEq86y5mD6e', 'Admin', NULL, 'admin', NULL, '082114578976', NULL, NULL, NULL, NULL, '2023-10-23 15:51:57', '2023-10-23 15:51:57', NULL, 1, NULL, NULL);
+INSERT INTO `users` VALUES (2, 'guru@test.test', '$2y$10$45x6uR6jJ2.gkDtfGzmPbO296miuAVhc0xbzhJxDmH19ob99bRlL2', 'Guru', NULL, 'guru', NULL, '082114578976', NULL, NULL, NULL, NULL, '2023-10-23 15:51:58', '2023-10-23 15:51:58', NULL, 1, NULL, NULL);
+INSERT INTO `users` VALUES (3, 'siswa@test.test', '$2y$10$eF91P0SYP0R.2Lxv3fYRqu7tMY.iBHWjO5CXgbNwkntRVHlcxIB4i', 'Siswa', '7', 'siswa', '123456789', '082114578976', '123456789', 'Orang Tua Siswa', '085603355799', NULL, '2023-10-23 15:51:58', '2023-10-23 15:51:58', NULL, 1, NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
